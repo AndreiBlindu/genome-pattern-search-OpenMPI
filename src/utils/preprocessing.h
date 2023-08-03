ce94136786fa5fb4b31a8edab623128a704cca6b
AndreiBlindu <andreiblindu@live.it>
2023-08-03 21:08:56 +0200

    bwt preprocessing

--- src/utils/preprocessing.h
+++ src/utils/preprocessing.h
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 char *preprocessing(char *text)
 {
@@ -8,11 +9,15 @@ char *preprocessing(char *text)
     char *newString = (char *)malloc(N);
     int j = 0;
 
-    for (int i = 1; i < N; i++)
+    for (int i = 0; i < N; i++)
     {
-        if (text[i] != '\n' && (text[i] == 'A' || text[i] == 'T' || text[i] == 'C' || text[i] == 'G'))
+        if (text[i] != '\n' && (toupper(text[i]) == 'A' || toupper(text[i]) == 'T' || toupper(text[i]) == 'C' || toupper(text[i]) == 'G'))
         {
-            newString[j++] = text[i];
+            newString[j++] = toupper(text[i]);
+        }
+        if (text[i] == '$') // necessary for bwt
+        {
+            newString[j++] = '$';
         }
     }
 
